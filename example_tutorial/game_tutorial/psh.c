c2e2a94840de1763b9054dd6cefe122cf519209c
Delcoii <sunghoon8585@naver.com>
2025-08-26 14:02:11 +0900

    practicing..

--- example_tutorial/game_tutorial/psh.c
+++ example_tutorial/game_tutorial/psh.c
@@ -71,6 +71,7 @@ void keyboard_init() {
     memset(key, 0, sizeof(key));
 }
 
+// 키 눌렸을 때, 떼었을 때에 대해 모든 키값을 비트연산으로 한 방에 처리 !!
 void keyboard_update(ALLEGRO_EVENT* event)
 {
     switch (event->type)
@@ -90,3 +91,399 @@ void keyboard_update(ALLEGRO_EVENT* event)
 }
 
 
+
+
+// --- sprites ---
+// spritepng에서 자신이 사용할 위치, 너비를 지정하여 가져옴
+//
+
+#define SHIP_W 12
+#define SHIP_H 13
+
+#define SHIP_SHOT_W 2
+#define SHIP_SHOT_H 9
+
+#define LIFE_W 6
+#define LIFE_H 6
+
+const int ALIEN_W[] = { 14, 13, 45 };
+const int ALIEN_H[] = { 9, 10, 27 };
+
+#define ALIEN_BUG_W      ALIEN_W[0]
+#define ALIEN_BUG_H      ALIEN_H[0]
+#define ALIEN_ARROW_W    ALIEN_W[1]
+#define ALIEN_ARROW_H    ALIEN_H[1]
+#define ALIEN_THICCBOI_W ALIEN_W[2]
+#define ALIEN_THICCBOI_H ALIEN_H[2]
+
+#define ALIEN_SHOT_W 4
+#define ALIEN_SHOT_H 4
+
+#define EXPLOSION_FRAMES 4
+#define SPARKS_FRAMES    3
+
+
+typedef struct SPRITES
+{
+    ALLEGRO_BITMAP* _sheet;
+
+    ALLEGRO_BITMAP* ship;
+    ALLEGRO_BITMAP* ship_shot[2]; // 2프레임 애니메이션
+    ALLEGRO_BITMAP* life;
+
+    ALLEGRO_BITMAP* alien[3];     // 3종류의 적
+    ALLEGRO_BITMAP* alien_shot;   // 적 총알
+
+    ALLEGRO_BITMAP* explosion[EXPLOSION_FRAMES];    // 폭발 효과
+    ALLEGRO_BITMAP* sparks[SPARKS_FRAMES];          // 스파크 효과
+
+    ALLEGRO_BITMAP* powerup[4];                     // 파워업 아이템
+} SPRITES;
+SPRITES sprites;
+
+
+
+ALLEGRO_BITMAP* sprite_grab(int x, int y, int w, int h)
+{
+    ALLEGRO_BITMAP* sprite = al_create_sub_bitmap(sprites._sheet, x, y, w, h);
+    must_init(sprite, "sprite grab");
+    return sprite;
+}
+
+void sprites_init()
+{
+    sprites._sheet = al_load_bitmap("spritesheet.png");
+    must_init(sprites._sheet, "spritesheet");
+
+    sprites.ship = sprite_grab(0, 0, SHIP_W, SHIP_H);
+
+    sprites.ship_shot[0] = sprite_grab(13, 0, SHIP_SHOT_W, SHIP_SHOT_H);
+    sprites.ship_shot[1] = sprite_grab(16, 0, SHIP_SHOT_W, SHIP_SHOT_H);
+
+    sprites.life = sprite_grab(0, 14, LIFE_W, LIFE_H);
+
+    sprites.alien[0] = sprite_grab(19, 0, ALIEN_BUG_W, ALIEN_BUG_H);
+    sprites.alien[1] = sprite_grab(19, 10, ALIEN_ARROW_W, ALIEN_ARROW_H);
+    sprites.alien[2] = sprite_grab(0, 21, ALIEN_THICCBOI_W, ALIEN_THICCBOI_H);
+
+    sprites.alien_shot = sprite_grab(13, 10, ALIEN_SHOT_W, ALIEN_SHOT_H);
+
+    sprites.explosion[0] = sprite_grab(33, 10, 9, 9);
+    sprites.explosion[1] = sprite_grab(43, 9, 11, 11);
+    sprites.explosion[2] = sprite_grab(46, 21, 17, 18);
+    sprites.explosion[3] = sprite_grab(46, 40, 17, 17);
+
+    sprites.sparks[0] = sprite_grab(34, 0, 10, 8);
+    sprites.sparks[1] = sprite_grab(45, 0, 7, 8);
+    sprites.sparks[2] = sprite_grab(54, 0, 9, 8);
+
+    sprites.powerup[0] = sprite_grab(0, 49, 9, 12);
+    sprites.powerup[1] = sprite_grab(10, 49, 9, 12);
+    sprites.powerup[2] = sprite_grab(20, 49, 9, 12);
+    sprites.powerup[3] = sprite_grab(30, 49, 9, 12);
+}
+
+void sprites_deinit()
+{
+    al_destroy_bitmap(sprites.ship);
+
+    al_destroy_bitmap(sprites.ship_shot[0]);
+    al_destroy_bitmap(sprites.ship_shot[1]);
+
+    al_destroy_bitmap(sprites.life);
+
+    al_destroy_bitmap(sprites.alien[0]);
+    al_destroy_bitmap(sprites.alien[1]);
+    al_destroy_bitmap(sprites.alien[2]);
+
+    al_destroy_bitmap(sprites.alien_shot);
+
+    al_destroy_bitmap(sprites.explosion[0]);
+    al_destroy_bitmap(sprites.explosion[1]);
+    al_destroy_bitmap(sprites.explosion[2]);
+    al_destroy_bitmap(sprites.explosion[3]);
+
+    al_destroy_bitmap(sprites.sparks[0]);
+    al_destroy_bitmap(sprites.sparks[1]);
+    al_destroy_bitmap(sprites.sparks[2]);
+
+    al_destroy_bitmap(sprites.powerup[0]);
+    al_destroy_bitmap(sprites.powerup[1]);
+    al_destroy_bitmap(sprites.powerup[2]);
+    al_destroy_bitmap(sprites.powerup[3]);
+
+    al_destroy_bitmap(sprites._sheet);
+}
+
+
+
+// --- audio ---
+
+ALLEGRO_SAMPLE* sample_shot;
+ALLEGRO_SAMPLE* sample_explode[2];
+
+void audio_init()
+{
+    al_install_audio();
+    al_init_acodec_addon();
+    al_reserve_samples(128);
+
+    sample_shot = al_load_sample("shot.flac");
+    must_init(sample_shot, "shot sample");
+
+    sample_explode[0] = al_load_sample("explode1.flac");
+    must_init(sample_explode[0], "explode[0] sample");
+    sample_explode[1] = al_load_sample("explode2.flac");
+    must_init(sample_explode[1], "explode[1] sample");
+}
+
+void audio_deinit()
+{
+    al_destroy_sample(sample_shot);
+    al_destroy_sample(sample_explode[0]);
+    al_destroy_sample(sample_explode[1]);
+}
+
+
+// --- fx ---
+
+typedef struct FX
+{
+    int x, y;
+    int frame;
+    bool spark;
+    bool used;
+} FX;
+
+#define FX_N 128
+FX fx[FX_N];
+
+void fx_init()
+{
+    for (int i = 0; i < FX_N; i++)
+        fx[i].used = false;
+}
+
+void fx_add(bool spark, int x, int y)
+{
+    if (!spark)
+        al_play_sample(sample_explode[between(0, 2)], 0.75, 0, 1, ALLEGRO_PLAYMODE_ONCE, NULL);
+
+    for (int i = 0; i < FX_N; i++)
+    {
+        if (fx[i].used)
+            continue;
+
+        fx[i].x = x;
+        fx[i].y = y;
+        fx[i].frame = 0;
+        fx[i].spark = spark;
+        fx[i].used = true;
+        return;
+    }
+}
+
+void fx_update()
+{
+    for (int i = 0; i < FX_N; i++)
+    {
+        if (!fx[i].used)
+            continue;
+
+        fx[i].frame++;
+
+        if ((!fx[i].spark && (fx[i].frame == (EXPLOSION_FRAMES * 2)))
+            || (fx[i].spark && (fx[i].frame == (SPARKS_FRAMES * 2)))
+            )
+            fx[i].used = false;
+    }
+}
+
+void fx_draw()
+{
+    for (int i = 0; i < FX_N; i++)
+    {
+        if (!fx[i].used)
+            continue;
+
+        int frame_display = fx[i].frame / 2;
+        ALLEGRO_BITMAP* bmp =
+            fx[i].spark
+            ? sprites.sparks[frame_display]
+            : sprites.explosion[frame_display]
+            ;
+
+        int x = fx[i].x - (al_get_bitmap_width(bmp) / 2);
+        int y = fx[i].y - (al_get_bitmap_height(bmp) / 2);
+        al_draw_bitmap(bmp, x, y, 0);
+    }
+}
+
+
+
+// --- shots ---
+
+typedef struct SHOT
+{
+    int x, y, dx, dy;
+    int frame;
+    bool ship;
+    bool used;
+} SHOT;
+
+#define SHOTS_N 128
+SHOT shots[SHOTS_N];
+
+void shots_init()
+{
+    for (int i = 0; i < SHOTS_N; i++)
+        shots[i].used = false;
+}
+
+bool shots_add(bool ship, bool straight, int x, int y)
+{
+    al_play_sample(
+        sample_shot,
+        0.3,
+        0,
+        ship ? 1.0 : between_f(1.5, 1.6),
+        ALLEGRO_PLAYMODE_ONCE,
+        NULL
+    );
+
+    for (int i = 0; i < SHOTS_N; i++)
+    {
+        if (shots[i].used)
+            continue;
+
+        shots[i].ship = ship;
+
+        if (ship)
+        {
+            shots[i].x = x - (SHIP_SHOT_W / 2);
+            shots[i].y = y;
+        }
+        else // alien
+        {
+            shots[i].x = x - (ALIEN_SHOT_W / 2);
+            shots[i].y = y - (ALIEN_SHOT_H / 2);
+
+            if (straight)
+            {
+                shots[i].dx = 0;
+                shots[i].dy = 2;
+            }
+            else
+            {
+
+                shots[i].dx = between(-2, 2);
+                shots[i].dy = between(-2, 2);
+            }
+
+            // if the shot has no speed, don't bother
+            if (!shots[i].dx && !shots[i].dy)
+                return true;
+
+            shots[i].frame = 0;
+        }
+
+        shots[i].frame = 0;
+        shots[i].used = true;
+
+        return true;
+    }
+    return false;
+}
+
+void shots_update()
+{
+    for (int i = 0; i < SHOTS_N; i++)
+    {
+        if (!shots[i].used)
+            continue;
+
+        if (shots[i].ship)
+        {
+            shots[i].y -= 5;
+
+            if (shots[i].y < -SHIP_SHOT_H)
+            {
+                shots[i].used = false;
+                continue;
+            }
+        }
+        else // alien
+        {
+            shots[i].x += shots[i].dx;
+            shots[i].y += shots[i].dy;
+
+            if ((shots[i].x < -ALIEN_SHOT_W)
+                || (shots[i].x > BUFFER_W)
+                || (shots[i].y < -ALIEN_SHOT_H)
+                || (shots[i].y > BUFFER_H)
+                ) {
+                shots[i].used = false;
+                continue;
+            }
+        }
+
+        shots[i].frame++;
+    }
+}
+
+bool shots_collide(bool ship, int x, int y, int w, int h)
+{
+    for (int i = 0; i < SHOTS_N; i++)
+    {
+        if (!shots[i].used)
+            continue;
+
+        // don't collide with one's own shots
+        if (shots[i].ship == ship)
+            continue;
+
+        int sw, sh;
+        if (ship)
+        {
+            sw = ALIEN_SHOT_W;
+            sh = ALIEN_SHOT_H;
+        }
+        else
+        {
+            sw = SHIP_SHOT_W;
+            sh = SHIP_SHOT_H;
+        }
+
+        if (collide(x, y, x + w, y + h, shots[i].x, shots[i].y, shots[i].x + sw, shots[i].y + sh))
+        {
+            fx_add(true, shots[i].x + (sw / 2), shots[i].y + (sh / 2));
+            shots[i].used = false;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void shots_draw()
+{
+    for (int i = 0; i < SHOTS_N; i++)
+    {
+        if (!shots[i].used)
+            continue;
+
+        int frame_display = (shots[i].frame / 2) % 2;
+
+        if (shots[i].ship)
+            al_draw_bitmap(sprites.ship_shot[frame_display], shots[i].x, shots[i].y, 0);
+        else // alien
+        {
+            ALLEGRO_COLOR tint =
+                frame_display
+                ? al_map_rgb_f(1, 1, 1)
+                : al_map_rgb_f(0.5, 0.5, 0.5)
+                ;
+            al_draw_tinted_bitmap(sprites.alien_shot, tint, shots[i].x, shots[i].y, 0);
+        }
+    }
+}
