e3a060c216be10776396f596355a3500810bc162
SeonghoonPark(Windows) <sunghoon8585@konkuk.ac.kr>
2025-08-26 21:18:30 +0900

    upload allegro package & empty proj

---
+++ TankBoy/main.c
@@ -0,0 +1,13 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_image.h>
+
+int main(void) {
+	return 0;
+}
